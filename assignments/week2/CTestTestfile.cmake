414a0dc04db599151d52781b7d200ba9d5c8b510
Philippe Zwietering <philippe.zwietering@gmail.com>
2019-01-12 02:46:19 +0100

    Removing unnecessary git tracking

--- assignments/week2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philippe/Documents/School/cpse2/assignments/week2/project
-# Build directory: /home/philippe/Documents/School/cpse2/assignments/week2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
