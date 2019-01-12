a87a743e2c1293062758a638edc006afab086152
Philippe Zwietering <philippe.zwietering@gmail.com>
2019-01-12 03:22:30 +0100

    Tidied up command line command

--- assignments/week5/project/main.cpp
+++ assignments/week5/project/main.cpp
@@ -3,7 +3,10 @@
 #include "game.hpp"
 
 int main(int argc, char *argv[]){
-    if(argc > 0){
+    if(argc <= 1){
+      std::cout << "Need an argument\n";
+    }
+    else if(argc > 1){
     	if(!strcmp(argv[1], "sfml")){
     		Game().runSFML();
     	} else if(!strcmp(argv[1], "text")){
